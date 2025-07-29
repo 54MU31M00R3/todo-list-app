b5850c7e32e97fa8ca4e7e81991576177d75391e
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2025-07-28 23:04:23 -0400

    Add module for viewing dashboard

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import './styles.css';
 import { homePage } from './pages/home.js';
 import { signupForm } from './forms/signupForm.js';
 import { loginForm } from './forms/loginForm.js';
+import { dashPage } from './pages/dashboard.js';
 
 // document.body.appendChild(homePage.getPage())
 // document.body.appendChild(signupForm.getForm())
 // document.body.appendChild(loginForm.getForm())
+
+document.body.appendChild(dashPage.getPage())
---
+++ src/pages/dashboard.js
@@ -0,0 +1,88 @@
+const projects = [
+    { name: "Online Classroom System", date: "2025-02-15" },
+    { name: "AI Development Research", date: "2025-03-12" },
+    { name: "PASS Program Gantt Chart", date: "2025-01-20" },
+    { name: "Cyberpunk Merch Website", date: "2025-07-19" },
+    { name: "Gelato Pop-Up Marketing", date: "2025-07-23" }
+  ];
+  
+
+const dashPage = (function () {
+    const getHeader = () => {
+        const header = document.createElement("div");
+        header.id = "header";
+
+        const title = document.createElement("div");
+        title.textContent = "Todo";
+        title.id = "app-title";
+        header.appendChild(title);
+
+        const nav = document.createElement("div");
+        nav.id = "nav-links";
+        header.appendChild(nav);
+
+        const homeButton = document.createElement("button");
+        homeButton.className = "nav-button"
+        homeButton.textContent = "Home"
+        nav.appendChild(homeButton);
+
+        const newProjectButton = document.createElement("button");
+        newProjectButton.className = "nav-button"
+        newProjectButton.textContent = "New Project"
+        nav.appendChild(newProjectButton);
+
+        return header;
+    }
+    const getCards = () => {
+        const cards = document.createElement("div");
+        cards.id = "cards";
+
+        const projectCard = document.createElement("div");
+        projectCard.className = "project-card";
+        cards.appendChild(projectCard);
+
+        const upperCard = document.createElement("div");
+        upperCard.className = "upper-card"
+        projectCard.appendChild(upperCard);
+
+        const projectTitle = document.createElement("div");
+        projectTitle.textContent = "Cottage Trip";
+        projectTitle.className = "project-title";
+        upperCard.appendChild(projectTitle);
+
+        const projectDate = document.createElement("div");
+        projectDate.textContent = "2023-11-23";
+        projectDate.className = "project-date";
+        upperCard.appendChild(projectDate);
+
+        const lowerCard = document.createElement("div");
+        lowerCard.className = "lower-card";
+        projectCard.appendChild(lowerCard);
+
+        const viewButton = document.createElement("button");
+        viewButton.className = "card-button";
+        viewButton.textContent = "View";
+        lowerCard.appendChild(viewButton);
+
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "card-button";
+        deleteButton.textContent = "Delete";
+        lowerCard.appendChild(deleteButton);
+
+        return cards;
+    }
+    const getPage = () => {
+        const page = document.createElement("div");
+        
+        const header = getHeader();
+        page.appendChild(header);
+
+        const cards = getCards();
+        page.appendChild(cards);
+
+        return page;
+    }
+    return { getPage };
+})();
+
+export { dashPage };
