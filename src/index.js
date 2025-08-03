b499ebb5b6f4e0bc399ca156abf6bd27d547b3b5
54MU31M00R3 <166854357+54MU31M00R3@users.noreply.github.com>
2025-08-03 17:18:31 -0400

    Add function to check for local storage option

--- src/index.js
+++ src/index.js
@@ -9,11 +9,34 @@ import { todoPage } from './pages/todo.js';
 import { newProjectForm } from './forms/newProjectForm.js';
 import { newTodoForm } from './forms/newTodoForm.js';
 
-// document.body.appendChild(homePage.getPage())
+function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  }
+
+if (storageAvailable("localStorage")) {
+    document.body.appendChild(homePage.getPage());
+} else {
+// Too bad, no localStorage for us
+}
+
 // document.body.appendChild(signupForm.getForm())
 // document.body.appendChild(loginForm.getForm())
 // document.body.appendChild(projectPage.getPage())
 // document.body.appendChild(todoPage.getPage())
 // document.body.appendChild(newProjectForm.getForm())
-
-document.body.appendChild(newTodoForm.getForm())
+// document.body.appendChild(newTodoForm.getForm())
